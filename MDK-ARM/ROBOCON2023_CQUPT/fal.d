52188178a93c8e1cecabe56e957e9250d57751ed
tengzumin <353265932@qq.com>
2023-06-03 12:17:06 +0800

    flash.2

--- MDK-ARM/ROBOCON2023_CQUPT/fal.d
+++ MDK-ARM/ROBOCON2023_CQUPT/fal.d
@@ -1,7 +1,7 @@
 robocon2023_cqupt\fal.o: ..\Hardware\flashbd\FAL\fal.c
-robocon2023_cqupt\fal.o: ..\..\FlashDB\port\fal\inc\fal.h
+robocon2023_cqupt\fal.o: ..\Hardware\flashbd\fal.h
 robocon2023_cqupt\fal.o: ..\Hardware\flashbd\fal_cfg.h
-robocon2023_cqupt\fal.o: ..\..\FlashDB\port\fal\inc\fal_def.h
-robocon2023_cqupt\fal.o: C:\Keil_v5\ARM\Compiler_5.06u7\Bin\..\include\stdint.h
-robocon2023_cqupt\fal.o: C:\Keil_v5\ARM\Compiler_5.06u7\Bin\..\include\stdio.h
-robocon2023_cqupt\fal.o: C:\Keil_v5\ARM\Compiler_5.06u7\Bin\..\include\inttypes.h
+robocon2023_cqupt\fal.o: ..\Hardware\flashbd\fal_def.h
+robocon2023_cqupt\fal.o: C:\keil\ARM\ARMCC\Bin\..\include\stdint.h
+robocon2023_cqupt\fal.o: C:\keil\ARM\ARMCC\Bin\..\include\stdio.h
+robocon2023_cqupt\fal.o: C:\keil\ARM\ARMCC\Bin\..\include\inttypes.h
